cdba99b7925671ba080763335eb4616d6c007ec0
gabrielacaesar <gabrielacaesar55@gmail.com>
2017-08-18 02:50:19 -0300

    learning beautifulsoap to help a friend

---
+++ scrapper17ago2017.py
@@ -0,0 +1,27 @@
+# my scrapper does work, but 
+# the whole content is on only one column
+
+# we should have six columns; check the website
+
+# we notice that the cells of the first column
+# always have the class "sorting_1"
+
+
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = "http://www.concursopublico.sp.gov.br/PortalConcurso/noauth/PortalDeConcursos.do?acao=concursoEncerrado"
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+page_soup = soup(page_html, "html.parser")
+
+rows = [[td.text 
+for td in tr.find_all('td')]
+for tr in page_soup.find_all("tr")]
+
+import csv
+with open('concurso.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(rows)
