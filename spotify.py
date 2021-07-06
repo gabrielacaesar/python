aedb463d94f54a591a9ca70201f582fbbd401d94
gabrielacaesar <gabrielacaesar55@gmail.com>
2021-07-06 15:55:52 -0300

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -5,5 +5,5 @@ browser = webdriver.Chrome(ChromeDriverManager().install())
 
 browser.get('https://spotifycharts.com/regional/br/daily/2020-07-02')
 
-botao = browser.find_element_by_class_name('header-csv')
+botao = browser.find_element_by_xpath('//a[@class="header-csv"]')
 botao.click()
